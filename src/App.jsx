35b311f0ff386facf6977076c1de8eae6a96610f
stefanharakal-byte <stefan.harakal@gmail.com>
2026-06-11 22:43:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // ===== NASTAVENÍ SUPABASE — vyplň anon klíč =====
 const SUPABASE_URL = 'https://bnkkimycxuqjzgdwgxll.supabase.co'
-const SUPABASE_ANON_KEY = 'https://bnkkimycxuqjzgdwgxll.supabase.co/rest/v1/'
+const SUPABASE_ANON_KEY = 'sb_publishable_lF6DhR0Dn-NJSGxlhyL1lA_bgGsVmS9'
 // =================================================
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
